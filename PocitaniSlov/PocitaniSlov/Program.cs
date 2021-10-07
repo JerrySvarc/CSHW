e237bd354e29090369ed613abd1f5ff9ef99e2b9
Jaroslav Švarc <jara.svarc@email.cz>
2021-10-07 22:31:24 +0200

    first homework - not working

---
+++ PocitaniSlov/PocitaniSlov/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace PocitaniSlov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            Counter counter = new Counter();
+            string[] lines;
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Argument Error");
+            }
+            else
+            {
+                lines = reader.GetInput(args[0]);
+                counter.CountWords(lines, counter);
+                Console.WriteLine(counter.GetOutput());
+            }
+
+        }
+
+    }
+
+    class Reader
+    {
+        public string[] GetInput(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            return lines;
+        }
+
+    }
+
+    class Counter
+    {
+        private int wordCount;
+        public int GetOutput()
+        {
+            return wordCount;
+        }
+
+        public void IncrementCounter()
+        {
+            wordCount += 1;
+        }
+
+        public void CountWords(string[] lines, Counter counter)
+        {
+            string[] words;
+            foreach (string line in lines)
+            {
+                line.Replace('\n', ' ');
+                line.Replace('\t', ' ');
+                words = line.Split(" ");
+
+                foreach (string word in words)
+                {
+                    counter.IncrementCounter();
+                }
+
+            }
+        }
+    }
+}
