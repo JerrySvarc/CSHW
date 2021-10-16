99c563b9dec58b415d0bf3729c8d953809b7a108
Jaroslav Švarc <jara.svarc@email.cz>
2021-10-16 23:37:28 +0200

    Made 2 interfaces + the outline for the TextWriter class. Will finish tomorrow.

--- BlokText/BlokText/Program.cs
+++ BlokText/BlokText/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace BlokText
 {
@@ -9,14 +7,21 @@ namespace BlokText
     {
         static void Main(string[] args)
         {
+            FileReader reader = new FileReader();
+            TextWriter writer = new TextWriter();
+
             if (args.Length != 3 || !int.TryParse(args[2], out int value) || value < 1)
             {
                 Console.WriteLine("Argument Error");
             }
+            else
+            {
+
+            }
         }
     }
 
-    public interface IFileReader
+    interface IFileReader
     {
         public StreamReader GetInput(string name);
         public char ReadChar(StreamReader reader);
@@ -24,7 +29,8 @@ namespace BlokText
 
     public interface ITextWriter
     {
-        public void WriteSpace();
+        public void CreateOutputFile(string name);
+        public void WriteText(string text, StreamWriter writer);
     }
 
     class FileReader : IFileReader
@@ -38,7 +44,6 @@ namespace BlokText
             try
             {
                 reader = new StreamReader(name);
-                
             }
             catch (IOException)
             {
@@ -47,13 +52,13 @@ namespace BlokText
             }
             return reader;
         }
-
+        // Reads one character from the input file 
         public char ReadChar(StreamReader reader)
         {
             try
             {
                 characterVal = reader.Read();
-                character = (char) characterVal;
+                character = (char)characterVal;
             }
             catch (IOException)
             {
@@ -62,7 +67,42 @@ namespace BlokText
             }
             return character;
         }
+
+
     }
 
-    
+    class TextWriter : ITextWriter
+    {
+        private StreamWriter writer;
+
+
+        public void CreateOutputFile(string name)
+        {
+            try
+            {
+                writer = new StreamWriter(name);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("File Error");
+                throw;
+            }
+            
+        }
+
+
+        public void WriteText(string text, StreamWriter writer)
+        {
+            writer.WriteLine(text);
+        }
+
+        public void FormatLine()
+        {
+
+        }
+    }
+
+
+
+
 }
