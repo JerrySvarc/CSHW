412e909b4c626fda28cfbd433ef1fbce321ac6c4
Jaroslav Švarc <jara.svarc@email.cz>
2021-11-22 08:32:07 +0100

    Hw - need to implement outputing coded file + performance optimatisation.

--- Huffman2/Huffman2/Program.cs
+++ Huffman2/Huffman2/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
+
 
 namespace Huffman2
 {
@@ -9,25 +10,40 @@ namespace Huffman2
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length != 1)
             {
                 Console.WriteLine("Argument Error");
+                return;
             }
             else
             {
                 try
                 {
-                    Outputer output = new Outputer(args[1]);
+                    string outputFile = args[0] + ".huff";
+                    Outputer output = new Outputer(outputFile);
                     Tree tree = new Tree();
                     PriorityQueue queue = new PriorityQueue();
                     Input input = new Input(args[0]);
                     Counter counter = new Counter();
+                    FileEncoder encoder = new FileEncoder();
+                    using (output.writer)
+                    {
+                        //TODO: Build it all together - finish main()
+                        tree.BuildTree(input, queue, counter);
+                        input.file.Close();
+                        input = new Input(args[0]);
+                        //write header
+                        output.WriteHeader();
+                        //write coded tree
+                        tree.OutputTree(output);
+                        output.WriteTreeEnd();
+                        //write encoded file contents
+                        List<bool> list = new List<bool>();
+                        encoder.FindCharCoding(tree.Root, list);
+                        output.WriteEncodedFile(encoder.GetCodingTemplates(), input);
 
-                    //TODO: Build it all together - finish main()
-                    tree.BuildTree(input, queue, counter);
+                    }
 
-
-                    tree.OutputTree(output);
                 }
                 catch (FileNotFoundException)
                 {
@@ -56,20 +72,55 @@ namespace Huffman2
 
     class Outputer
     {
-        BinaryWriter writer;
+        public BinaryWriter writer;
+        byte[] result = new byte[1];
+        List<bool> buffer = new List<bool>(1024);
+        BitArray bits = new BitArray(8);
         public Outputer(string fileName)
         {
-            writer = new BinaryWriter(new FileStream(fileName, FileMode.Create));
+            writer = new BinaryWriter(new FileStream(fileName, FileMode.OpenOrCreate));
         }
         //TODO: Implement writing output
-        public void WriteOutput(byte value, BinaryWriter writer)
+        public void WriteEncodedFile(Dictionary<byte, List<bool>> templates, Input input)
         {
-
             
-
-
+        }
+        public void WriteEncodedByte(BitArray bits)
+        {
+            bits.CopyTo(result, 0);
+            writer.Write(result);
         }
 
+        public void WriteTreeEnd()
+        {
+            byte[] footer = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            writer.Write(footer);
+        }
+        public void WriteHeader()
+        {
+            byte[] header = new byte[] { 0x7B, 0x68, 0x75, 0x7C, 0x6D, 0x7D, 0x66, 0x66 };
+            writer.Write(header);
+        }
+
+        public void WriteInner(Node node)
+        {
+            long result = 0;
+            long weight = node.Weight;
+            weight = weight & 0x00007FFFFFFFFFFF;
+            result = weight << 1;
+            writer.Write(result);
+        }
+
+        public void WriteLeaf(Node node)
+        {
+            long result = 0;
+            long weight = node.Weight;
+            byte symbol = (byte)node.Symbol;
+            weight = ((weight & 0x00007FFFFFFFFFFF) << 1) + 1;
+            result = (result | symbol) << 56;
+            result |= weight;
+            writer.Write(result);
+        }
         public void WriteTree(Node node)
         {
             if (node == null)
@@ -77,22 +128,26 @@ namespace Huffman2
                 return;
             }
 
-            if (node.Symbol != -1)
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(node);
+
+            while (stack.Count > 0)
             {
-                Console.Write("*" + node.Symbol + ":" + node.Weight + " ");
-            }
-            else
-            {
-                Console.Write(node.Weight + " ");
+                Node currNode = stack.Pop();
+                if (currNode.Symbol != -1)
+                {
+                    WriteLeaf(currNode);
+                }
+                else
+                {
+                    WriteInner(currNode);
+                    stack.Push(currNode.RightChild);
+                    stack.Push(currNode.LeftChild);
+                }
+
             }
 
-            WriteTree(node.LeftChild);
-            WriteTree(node.RightChild);
         }
-
-
-
-
     }
 
     class Counter
@@ -163,14 +218,16 @@ namespace Huffman2
 
     class Tree
     {
-        public HashSet<Node> tree = new HashSet<Node>();
-        
-        public Node Root;
-        public void AddNode(Node node)
-        {
-            tree.Add(node);
-        }
 
+
+        public Node Root;
+
+        /// <summary>
+        /// Builds a Huffman tree from Input file. Inner nodes have a symbol value of "-1". 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="queue"></param>
+        /// <param name="counter"></param>
         public void BuildTree(Input input, PriorityQueue queue, Counter counter)
         {
             counter.LoadQueue(counter.GetSymbolCount(input), queue);
@@ -181,9 +238,6 @@ namespace Huffman2
                 Node right = queue.Dequeue();
                 ++turn;
                 Node newNode = new Node(left, right, -1, left.Weight + right.Weight, turn);
-                tree.Add(left);
-                tree.Add(right);
-                tree.Add(newNode);
                 queue.Queue(newNode, newNode.Weight);
             }
 
@@ -198,10 +252,14 @@ namespace Huffman2
 
     class PriorityQueue
     {
+        /// <summary>
+        /// A priority queue implementation using a SortedSet with a custom comparer. 
+        /// </summary>
         private SortedSet<Tuple<Node, int>>
             heap = new SortedSet<Tuple<Node, int>>(
                 Comparer<Tuple<Node, int>>.Create(
-                    (x, y) => {
+                    (x, y) =>
+                    {
                         if (x.Item2 == y.Item2)
                         {
                             if (x.Item1.Turn == 0 && y.Item1.Turn != 0)
@@ -241,14 +299,24 @@ namespace Huffman2
                     }
                 ));
 
+        /// <summary>
+        /// Removes item from the top of the priotity queue.
+        /// </summary>
+        /// <returns></returns>
         public Node Dequeue()
         {
             using IEnumerator<Tuple<Node, int>> enumerator = heap.GetEnumerator();
             enumerator.MoveNext();
             heap.Remove(enumerator.Current);
             return enumerator.Current.Item1;
-        }
 
+
+        }
+        /// <summary>
+        /// Adds item to the priority queue.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="weight"></param>
         public void Queue(Node node, int weight)
         {
             heap.Add(new Tuple<Node, int>(node, weight));
@@ -259,14 +327,23 @@ namespace Huffman2
             return heap.Count;
         }
     }
-    //TODO: implement building output bytes
-    class ByteBuilder
+
+    class FileEncoder
     {
+        //TODO: IMPLEMENT ME!
 
+        /// <summary>
+        /// Recursively find encoding template for all leaves.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="bits"></param>
+        public void FindCharCoding(Node node, List<bool> bits)
+        {
 
-
-
-
+        }
+        public Dictionary<byte, List<bool>> GetCodingTemplates()
+        {
+            //return codingTemplates;
+        }
     }
-
 }
